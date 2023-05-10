6f6faf796b4fe30b02b9d8a6bb576c30ec1146a8
amber-king <amberking@pursuit.org>
2023-05-10 13:30:21 -0400

    snack index can be seen & css set up done

---
+++ src/Components/Snack.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+function Snack({ snack }) {
+  return (
+    <tr>
+      <td className="Snack">
+        <a href={snack.snack_name} target="_blank" rel="noreferrer">
+          {snack.snack_name}
+        </a>
+      </td>
+      <td>{snack.time_eaten}</td>
+      <td>{snack.food_group}</td>
+      <td>{snack.is_healthy ? <span>❤️</span> : <span>🫤</span>}</td>
+
+      <td>{snack.calorie_count}</td>
+      <td>{snack.quantity}</td>
+
+      <td>
+        <Link to={`/snacks/${snack.snack_id}`}></Link>
+      </td>
+    </tr>
+  );
+}
+
+export default Snack;
