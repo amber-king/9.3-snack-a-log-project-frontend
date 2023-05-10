fd4b728df41e8033379e29d20d2c6c8eac1b5fb8
amber-king <amberking@pursuit.org>
2023-05-10 15:52:26 -0400

    modified:   src/Components/Snack.js

--- src/Components/Snack.js
+++ src/Components/Snack.js
@@ -1,4 +1,4 @@
-// TODO: http://localhost:3033/snacks -> connects to Snack.js;snack,js will NOT work w/o it
+// TODO: http://localhost:3033/snacks -> connects to Snacks.js;snack,js will NOT work w/o it
 import { Link } from "react-router-dom";
 
 function Snack({ snack }) {
